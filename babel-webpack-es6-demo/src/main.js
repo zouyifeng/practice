601211acbb676e5c755ee921291e0e514e149282
zouyifeng <zouyifeng@aliyun.com>
2016-11-20 09:58:10 +0800

    bugfix

---
+++ babel-webpack-es6-demo/src/main.js
@@ -0,0 +1,17 @@
+import pokemonGif from 'pokemon-gif'
+
+const btn = document.getElementsByClassName('btn')[0]
+
+btn.addEventListener('click', () => {
+	console.log('Clicked!');
+	const randomId = Math.ceil(Math.random()*721)
+	const gifUrl = pokemonGif(randomId)
+	console.log(gifUrl)
+	if(gifUrl) {
+		const img = document.creatElement('img')
+		img.src = gifUrl;
+		const body = document.getElementsByTagName('body')[0]
+		body.removeChild(btn)
+		body.appendChild(img)
+	}
+})
