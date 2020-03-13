598555dc94ca17d8f7e600f3d213a47e931203d8
zouyifeng <zouyifeng@aliyun.com>
2020-03-13 23:15:45 +0800

    二叉树遍历算法补充

--- js/数据结构/二叉树/index.js
+++ js/数据结构/二叉树/index.js
@@ -33,7 +33,7 @@ var tree = {
 
 // 前序遍历 递归
 var prelistRec = []
-var preOrderRec = function(node) {
+var preOrderRec = function (node) {
   if (node) {
     prelistRec.push(node.value)
     preOrderRec(node.left)
@@ -41,7 +41,7 @@ var preOrderRec = function(node) {
   }
 }
 preOrderRec(tree)
-console.log(prelistRec)
+console.log('前序遍历 递归', prelistRec)
 
 
 // 前序遍历，非递归
@@ -59,10 +59,11 @@ var preOrderUnRecursion = function (node) {
 }
 
 preOrderUnRecursion(tree)
-console.log(preListUnRec)
+console.log('前序遍历，非递归', preListUnRec)
 
 var inListRec = []
 
+// 中序遍历
 var inOrderRec = function (node) {
   if (node) {
     inOrderRec(node.left)
@@ -71,99 +72,92 @@ var inOrderRec = function (node) {
   }
 }
 inOrderRec(tree)
-console.log('inListRec: ', inListRec);
+console.log('中序遍历: ', inListRec);
 
+// 中序遍历
 var inListUnRec = []; //定义保存中序遍历结果的数组
-var inOrderUnRec = function(node) {
-    if (node) { //判断二叉树是否为空
-        var stack = []; //建立一个栈
-        while (stack.length !== 0 || node) { //如果栈不为空或结点不为空，则循环遍历
-            if (node) { //如果结点不为空
-                stack.push(node); //将结点压入栈
-                node = node.left; //将左子树作为当前结点
-            } else { //左子树为空，即没有左子树的情况
-                node = stack.pop(); //将结点取出来
-                inListUnRec.push(node.value); //将取出结点的值存入数组中
-                node = node.right; //将右结点作为当前结点
-            }
-        }
+var inOrderUnRec = function (node) {
+  if (node) { //判断二叉树是否为空
+    var stack = []; //建立一个栈
+    while (stack.length !== 0 || node) { //如果栈不为空或结点不为空，则循环遍历
+      if (node) { //如果结点不为空
+        stack.push(node); //将结点压入栈
+        node = node.left; //将左子树作为当前结点
+      } else { //左子树为空，即没有左子树的情况
+        node = stack.pop(); //将结点取出来
+        inListUnRec.push(node.value); //将取出结点的值存入数组中
+        node = node.right; //将右结点作为当前结点
+      }
     }
+  }
 }
 inOrderUnRec(tree);
-console.log('inListUnRec: ', inListUnRec);
+console.log('中序遍历: ', inListUnRec);
 
+// 后序遍历
 var postListRec = []; //定义保存后序遍历结果的数组
-var postOrderRec = function(node) {
-    if (node) { //判断二叉树是否为空
-        postOrderRec(node.left); //递归遍历左子树
-        postOrderRec(node.right); //递归遍历右子树
-        postListRec.push(node.value); //将结点的值存入数组中
-    }
+var postOrderRec = function (node) {
+  if (node) { //判断二叉树是否为空
+    postOrderRec(node.left); //递归遍历左子树
+    postOrderRec(node.right); //递归遍历右子树
+    postListRec.push(node.value); //将结点的值存入数组中
+  }
 }
 postOrderRec(tree);
-console.log(postListRec);
+console.log('后序遍历', postListRec);
 
-var postListRec = []; //定义保存后序遍历结果的数组
-var postOrderRec = function(node) {
-    if (node) { //判断二叉树是否为空
-        postOrderRec(node.left); //递归遍历左子树
-        postOrderRec(node.right); //递归遍历右子树
-        postListRec.push(node.value); //将结点的值存入数组中
-    }
-}
-postOrderRec(tree);
-console.log(postListRec);
-
+// 后序遍历
 var postListUnRec = []; //定义保存后序遍历结果的数组
-var postOrderUnRec = function(node) {
-    if (node) { //判断二叉树是否为空
-        var stack = [node]; //将二叉树压入栈
-        var tmp = null; //定义缓存变量
-        while (stack.length !== 0) { //如果栈不为空，则循环遍历
-            tmp = stack[stack.length - 1]; //将栈顶的值保存在tmp中
-            if (tmp.left && node !== tmp.left && node !== tmp.right) { //如果存在左子树
-                stack.push(tmp.left); //将左子树结点压入栈
-            } else if (tmp.right && node !== tmp.right) { //如果结点存在右子树
-                stack.push(tmp.right); //将右子树压入栈中
-            } else {
-                postListUnRec.push(stack.pop().value);
-                node = tmp;
-            }
-        }
+var postOrderUnRec = function (node) {
+  if (node) { //判断二叉树是否为空
+    var stack = [node]; //将二叉树压入栈
+    var tmp = null; //定义缓存变量
+    while (stack.length !== 0) { //如果栈不为空，则循环遍历
+      tmp = stack[stack.length - 1]; //将栈顶的值保存在tmp中
+      if (tmp.left && node !== tmp.left && node !== tmp.right) { //如果存在左子树
+        stack.push(tmp.left); //将左子树结点压入栈
+      } else if (tmp.right && node !== tmp.right) { //如果结点存在右子树
+        stack.push(tmp.right); //将右子树压入栈中
+      } else {
+        postListUnRec.push(stack.pop().value);
+        node = tmp;
+      }
     }
+  }
 }
 postOrderUnRec(tree);
-console.log(postListUnRec);
+console.log('后序遍历', postListUnRec);
 
+// 广度遍历
 var breadthList = []; //定义保存广度遍历结果的数组
-var breadthTraversal = function(node) {
-    if (node) { //判断二叉树是否为空
-        var que = [node]; //将二叉树放入队列
-        while (que.length !== 0) { //判断队列是否为空
-            node = que.shift(); //从队列中取出一个结点
-            breadthList.push(node.value); //将取出结点的值保存到数组
-            if (node.left) que.push(node.left); //如果存在左子树，将左子树放入队列
-            if (node.right) que.push(node.right); //如果存在右子树，将右子树放入队列
-        }
+var breadthTraversal = function (node) {
+  if (node) { //判断二叉树是否为空
+    var que = [node]; //将二叉树放入队列
+    while (que.length !== 0) { //判断队列是否为空
+      node = que.shift(); //从队列中取出一个结点
+      breadthList.push(node.value); //将取出结点的值保存到数组
+      if (node.left) que.push(node.left); //如果存在左子树，将左子树放入队列
+      if (node.right) que.push(node.right); //如果存在右子树，将右子树放入队列
     }
+  }
 }
 breadthTraversal(tree);
-console.log(breadthList);
+console.log('广度遍历', breadthList);
 
 // 翻转二叉树
-var invertTree = function (root) {
-  if (root) {
-    var temp = root.left
-    // if (root.left && root.right) {
-      root.left = root.right
-      root.right = temp
-    // }
-    invertTree(root.left)
-    invertTree(root.right)
+;(function () {
+  var invertTree = function (root) {
+    if (root) {
+      var temp = root.left;
+      // if (root.left && root.right) {
+      root.left = root.right;
+      root.right = temp;
+      // }
+      invertTree(root.left);
+      invertTree(root.right);
+    }
+    return root;
   }
-  return root
-}
-
-console.log(invertTree(tree))
-
-
+  
+  console.log('翻转二叉树', invertTree(tree))
+})()
