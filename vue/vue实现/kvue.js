ab09e5a053c438d773aff5bc3bdc0413eb3a19ab
zouyifeng <zouyifeng@aliyun.com>
2020-09-07 09:52:06 +0800

    vue

--- vue/vue实现/kvue.js
+++ vue/vue实现/kvue.js
@@ -1,3 +1,6 @@
+// Vue通过数据绑定来修改视图，当某个数据被修改的时候，
+// set方法会让闭包中的Dep调用notify通知所有订阅者Watcher，
+// Watcher通过get方法执行vm._update(vm._render(), hydrating)。
 class KVue {
   constructor(options) {
       this.$data = options.data; // 保存data选项
